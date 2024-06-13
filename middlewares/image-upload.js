f3433db6db0c6aa3c3e631fc37a2023f083c3ec4
Saksham BASSI <bassisaksham2222@gmail.com>
2024-06-14 03:44:43 +0530

    f1

--- middlewares/image-upload.js
+++ middlewares/image-upload.js
@@ -1,22 +1,38 @@
+// const multer = require('multer');
+// const uuid = require('uuid').v4;
+// const os = require('os');
+// const path = require('path');
+// const mkdirp = require('mkdirp');
+
+// // Get the temporary directory provided by Vercel
+// const tempDir = os.tmpdir();
+
+// const upload = multer({
+//   storage: multer.diskStorage({
+//     destination: function (req, file, cb) {
+//       // Use the temporary directory for storing files
+//       const uploadPath = path.join(tempDir, 'product-data/images');
+//       // Ensure the directory exists
+//       mkdirp.sync(uploadPath);
+//       cb(null, uploadPath);
+//     },
+//     filename: function (req, file, cb) {
+//       cb(null, uuid() + '-' + file.originalname);
+//     }
+//   })
+// });
+
+// const configuredMulterMiddleware = upload.single('image');
+
+// module.exports = configuredMulterMiddleware;
+
 const multer = require('multer');
 const uuid = require('uuid').v4;
-const os = require('os');
-const path = require('path');
-const mkdirp = require('mkdirp');
-
-// Get the temporary directory provided by Vercel
-const tempDir = os.tmpdir();
 
 const upload = multer({
   storage: multer.diskStorage({
-    destination: function (req, file, cb) {
-      // Use the temporary directory for storing files
-      const uploadPath = path.join(tempDir, 'product-data/images');
-      // Ensure the directory exists
-      mkdirp.sync(uploadPath);
-      cb(null, uploadPath);
-    },
-    filename: function (req, file, cb) {
+    destination: 'product-data/images',
+    filename: function(req, file, cb) {
       cb(null, uuid() + '-' + file.originalname);
     }
   })
