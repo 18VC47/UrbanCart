e107dede09d4d4a2c54f8d901f1a2c20df1a59e6
Saksham BASSI <bassisaksham2222@gmail.com>
2024-06-14 00:56:51 +0530

    initial

--- data/database.js
+++ data/database.js
@@ -4,14 +4,14 @@ const MongoClient = mongodb.MongoClient;
 
 let database;
 
-let Mongo = 'mongodb://127.0.0.1:27017';
+// let Mongo = 'mongodb://127.0.0.1:27017';
 
-if(process.env.MONOGO_URL){
-  Mongo = MONOGO_URL;
-}
+// if(process.env.MONOGO_URL){
+//   Mongo = MONOGO_URL;
+// }
 
 async function connectToDatabase() {
-  const client = await MongoClient.connect(Mongo);
+  const client = await MongoClient.connect(MONOGO_URL);
   database = client.db('online-shop');
 }
 
